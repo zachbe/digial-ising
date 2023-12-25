48667329ec36cd9945db6c46197125af73222c53
zbelateche <zachbela@stanford.edu>
2023-12-24 23:17:05 -0800

    Parameterized core matrix done

--- rtl/cells/core_matrix.v
+++ rtl/cells/core_matrix.v
@@ -2,6 +2,8 @@
 
 // Create an NxN array of coupled cells
 //
+// Example with N = 4:
+//
 //       3|    2|    1|    0|
 //        |     |     |     |
 //    3   |  0  |  1  |  2  |  3
@@ -33,29 +35,35 @@ module core_matrix #(parameter N = 3,
 	             parameter NUM_WEIGHTS = 5,
 	             parameter WIRE_DELAY = 20) (
 		     input  wire rstn,
-		     input  wire [$clog2(NUM_WEIGHTS)-1:0] weights [(N*(N-1)/2)-1:0],
+		     input  wire [($clog2(NUM_WEIGHTS)*(N*(N-1)/2))-1:0] weights,
 		     output wire [N-1:0] outputs
 	            );
 
     genvar i;
     genvar j;
 
-    wire [N-1:0] osc_hor_in  [N-1:0];
-    wire [N-1:0] osc_ver_in  [N-1:0];
+    reg  [N-1:0] osc_hor_in  [N-1:0];
+    reg  [N-1:0] osc_ver_in  [N-1:0];
     wire [N-1:0] osc_hor_out [N-1:0];
     wire [N-1:0] osc_ver_out [N-1:0];
 
-    // Create the shorted cells
+    // Get outputs at the bottom of the array
+    // TODO: Figure out how to actually do stuff with these
     generate for (i = 0 ; i < N; i = i + 1) begin
-        shorted_cell i_short(.sin (rstn ? osc_hor_in[N-1] : 1'b0),
-		             .din (rstn ? osc_ver_in[N-1] : 1'b0),
-			     .sout(osc_hor_out[0]),
-			     .dout(osc_ver_out[0]));
+        assign outputs[i] = osc_ver_out[i][N-i-1];
     end endgenerate
 
-    // Create the right half of the coupled cells
+    // Create the shorted cells
     generate for (i = 0 ; i < N; i = i + 1) begin
-	generate for (j = i+1 ; j < N; j = j + 1) begin
+        shorted_cell i_short(.sin (rstn ? osc_hor_in[i][N-1] : 1'b0),
+		             .din (rstn ? osc_ver_in[i][N-1] : 1'b0),
+			     .sout(osc_hor_out[i][0]),
+			     .dout(osc_ver_out[i][0]));
+    end endgenerate
+
+    // Create the coupled cells
+    generate for (i = 0 ; i < N; i = i + 1) begin
+	for (j = i+1 ; j < N; j = j + 1) begin
 	    // Weights are in the order:
 	    // (0  , 1  ) (0  , 2) ... (0, N-1) (0, N)
 	    // (1  , 2  ) (1  , 3) ... (1, N  )
@@ -63,21 +71,48 @@ module core_matrix #(parameter N = 3,
 	    // (N-2, N-1) (N-2, N)
 	    // (N-1, N  )
 	    //
-	    // So, weight (i,j) is at index (N*i + (i*(i+1)/2) + j - i - 1)
-            wire weight_ij;
-	    assign weight_ij = weights[(N*i + (i*(i+1)/2) + j - i - 1)]
+	    // So, weight (i,j) is at index (N*i - (i*(i+1)/2) + j - i - 1)
+            wire [$clog2(NUM_WEIGHTS)-1:0] weight_ij;
+	    assign weight_ij = weights[((N*i - (i*(i+1)/2) + j - i - 1) 
+	                                 * $clog2(NUM_WEIGHTS)) 
+					 + $clog2(NUM_WEIGHTS) - 1    :
+	                                ((N*i - (i*(i+1)/2) + j - i - 1) 
+	                                 * $clog2(NUM_WEIGHTS))];
 
 	    // See top of file for wire indexing.
+	    //
+	    // Right half:
 	    // Horizontal wire input index is j-i-1.
 	    // Horiztonal wire output index is j-i.
-	    // Vertical wire input index is ??? 
+	    // Vertical wire input index is N-(j-i)-1
+	    // Vertical wire output index is N-(j-i) 
             coupled_cell ij_right(.weight(weight_ij),
-                                  .sin   (osc_ver_in[j-i-1]),
-                                  .din   (osc_hor_in[j-i]),
-                                  .sout  (osc_ver_out[1]),
-                                  .dout  (osc_hor_out[2]));
+                                  .sin   (osc_ver_in [j][N-(j-i)-1]),
+                                  .din   (osc_hor_in [i][j-i-1]),
+                                  .sout  (osc_ver_out[j][N-(j-i)]),
+                                  .dout  (osc_hor_out[i][j-i]));
+	    // Left half:
+	    // Vertical wire input index is j-i-1.
+	    // Vertical wire output index is j-i.
+	    // Horizontal wire input index is N-(j-i)-1
+	    // Horizontal wire output index is N-(j-i) 
+            coupled_cell ij_left (.weight(weight_ij),
+                                  .sin   (osc_ver_in [i][j-i-1]),
+                                  .din   (osc_hor_in [j][N-(j-i)-1]),
+                                  .sout  (osc_ver_out[i][j-i]),
+                                  .dout  (osc_hor_out[j][N-(j-i)]));
  
-	end endgenerate
+	end 
+    end endgenerate
+
+    // Add delays
+    generate for (i = 0 ; i < N; i = i + 1) begin
+	always @(osc_hor_out[i]) begin
+            #WIRE_DELAY osc_hor_in[i] <= osc_hor_out[i];
+	end
+	always @(osc_ver_out[i]) begin
+            #WIRE_DELAY osc_ver_in[i] <= osc_ver_out[i];
+	end
     end endgenerate
 
 endmodule
--- rtl/tb/couple.v
+++ rtl/tb/couple.v
@@ -1,110 +1,27 @@
 
-`include "../cells/coupled_cell.v"
-`include "../cells/shorted_cell.v"
+`include "../cells/core_matrix.v"
 
 `timescale 1ns/1ps
 module couple_tb();
 
-    reg rstn;
+    reg        rstn;
+
+    reg  [2:0] ab_weight;
+    reg  [2:0] bc_weight;
+    reg  [2:0] ac_weight;
+
+    wire [2:0] outputs;
+    
+    wire [8:0] weights;
+    assign     weights = {bc_weight, ac_weight, ab_weight};
 
     // Create a 3x3 array of coupled cells
-    //
-    //  0 | 1 | 2 | 0
-    //  --S---C---C--M
-    //  1 |   |   |
-    //  --C---S---C--M
-    //  2 |   |   |
-    //  --C---C---S--M
-    //  0 |   |   |
-    //    M   M   M
-    //
-    reg  [2:0] osc_a_hor_in;
-    reg  [2:0] osc_a_ver_in;
-    reg  [2:0] osc_b_hor_in;
-    reg  [2:0] osc_b_ver_in;
-    reg  [2:0] osc_c_hor_in;
-    reg  [2:0] osc_c_ver_in;
-    wire [2:0] osc_a_hor_out;
-    wire [2:0] osc_a_ver_out;
-    wire [2:0] osc_b_hor_out;
-    wire [2:0] osc_b_ver_out;
-    wire [2:0] osc_c_hor_out;
-    wire [2:0] osc_c_ver_out;
-
-    reg [2:0] ab_weight;
-    reg [2:0] bc_weight;
-    reg [2:0] ac_weight;
-
-    reg rst_a, rst_b, rst_c;
-
-    // 3 shorted cells
-    shorted_cell a_short(.sin (rstn ? osc_a_hor_in[0] : rst_a),
-	                 .din (rstn ? osc_a_ver_in[0] : rst_a),
-                         .sout(osc_a_hor_out[1]),
-			 .dout(osc_a_ver_out[1]));
-    shorted_cell b_short(.sin (rstn ? osc_b_hor_in[1] : rst_b),
-	                 .din (rstn ? osc_b_ver_in[1] : rst_b),
-                         .sout(osc_b_hor_out[2]),
-			 .dout(osc_b_ver_out[2]));
-    shorted_cell c_short(.sin (rstn ? osc_c_hor_in[2] : rst_c),
-	                 .din (rstn ? osc_c_ver_in[2] : rst_c),
-                         .sout(osc_c_hor_out[0]),
-			 .dout(osc_c_ver_out[0]));
-
-    // A x B coupling
-    coupled_cell ab_right(.weight(ab_weight),
-	                  .sin   (osc_b_ver_in[0]),
-			  .din   (osc_a_hor_in[1]),
-			  .sout  (osc_b_ver_out[1]),
-			  .dout  (osc_a_hor_out[2]));
-    coupled_cell ab_left (.weight(ab_weight),
-	                  .sin   (osc_b_hor_in[0]),
-			  .din   (osc_a_ver_in[1]),
-			  .sout  (osc_b_hor_out[1]),
-			  .dout  (osc_a_ver_out[2]));
-
-    // A x C coupling
-    coupled_cell ac_right(.weight(ac_weight),
-	                  .sin   (osc_a_hor_in[2]),
-			  .din   (osc_c_ver_in[0]),
-			  .sout  (osc_a_hor_out[0]),
-			  .dout  (osc_c_ver_out[1]));
-    coupled_cell ac_left (.weight(ac_weight),
-	                  .sin   (osc_a_ver_in[2]),
-			  .din   (osc_c_hor_in[0]),
-			  .sout  (osc_a_ver_out[0]),
-			  .dout  (osc_c_hor_out[1]));
-
-    // B x C coupling
-    coupled_cell bc_right(.weight(bc_weight),
-	                  .sin   (osc_c_ver_in[1]),
-			  .din   (osc_b_hor_in[2]),
-			  .sout  (osc_c_ver_out[2]),
-			  .dout  (osc_b_hor_out[0]));
-    coupled_cell bc_left (.weight(bc_weight),
-	                  .sin   (osc_c_hor_in[1]),
-			  .din   (osc_b_ver_in[2]),
-			  .sout  (osc_c_hor_out[2]),
-			  .dout  (osc_b_ver_out[0]));
-
-    always @(osc_a_hor_out) begin
-        #20 osc_a_hor_in <= osc_a_hor_out;  
-    end
-    always @(osc_b_hor_out) begin
-        #20 osc_b_hor_in <= osc_b_hor_out;  
-    end
-    always @(osc_c_hor_out) begin
-        #20 osc_c_hor_in <= osc_c_hor_out;  
-    end
-    always @(osc_a_ver_out) begin
-        #20 osc_a_ver_in <= osc_a_ver_out;  
-    end
-    always @(osc_b_ver_out) begin
-        #20 osc_b_ver_in <= osc_b_ver_out;  
-    end
-    always @(osc_c_ver_out) begin
-        #20 osc_c_ver_in <= osc_c_ver_out;  
-    end
+    core_matrix #(.N(3),
+	          .NUM_WEIGHTS(5),
+		  .WIRE_DELAY(20)) dut(
+		  .rstn(rstn),
+		  .weights(weights),
+		  .outputs(outputs));
 
     initial begin
 	$dumpfile("couple.vcd");
@@ -113,10 +30,6 @@ module couple_tb();
 	ab_weight = 3'b100; // couple A and B positively
 	bc_weight = 3'b000; // couple C and B negatively
 	ac_weight = 3'b010; // Dont couple A and C
-
-        rst_a = 1'b1;
-        rst_b = 1'b1;
-        rst_c = 1'b1;
 	
         rstn = 1'b0;	
 	#200;
