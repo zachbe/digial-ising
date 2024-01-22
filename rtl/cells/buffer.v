310e380fb96c6b87f67dff551451fe21468fedf2
zbelateche <zachbela@stanford.edu>
2024-01-21 20:09:32 -0800

    More buffer fixes

--- rtl/cells/buffer.v
+++ rtl/cells/buffer.v
@@ -15,10 +15,10 @@ module buffer #(parameter NUM_LUTS = 2)
         end
     `else
 	wire [NUM_LUTS-1 : 0] lut_out;
-        (* dont_touch = "yes" *) LUT1 #(.INIT(2'b10)) buf_lut (.I0(in), .O(lut_out[0]));
+        (* dont_touch = "yes" *) LUT1 #(.INIT(2'b10)) buf_lut_0 (.I0(in), .O(lut_out[0]));
         genvar i;
 	generate for (i = 1; i < NUM_LUTS; i = i+1) begin	
-            (* dont_touch = "yes" *) LUT1 #(.INIT(2'b10)) buf_lut (.I0(lut_out[i-1]). O(lut_out[i]));
+            (* dont_touch = "yes" *) LUT1 #(.INIT(2'b10)) buf_lut_i (.I0(lut_out[i-1]), .O(lut_out[i]));
 	end endgenerate
 	assign out = lut_out[NUM_LUTS-1];
     `endif
