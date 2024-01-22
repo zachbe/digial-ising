0de660fae6fec8bf7f9dde469d10f6661879c31a
zbelateche <zachbela@stanford.edu>
2024-01-21 19:54:58 -0800

    Fixed buffer cell param names

--- rtl/cells/buffer.v
+++ rtl/cells/buffer.v
@@ -14,13 +14,13 @@ module buffer #(parameter NUM_LUTS = 2)
             #1 o_reg = in;
         end
     `else
-	wire [NUM_LUT-1 : 0] lut_out;
+	wire [NUM_LUTS-1 : 0] lut_out;
         (* dont_touch = "yes" *) LUT1 #(.INIT(2'b10)) buf_lut (.I0(in), .O(lut_out[0]));
         genvar i;
-	generate for (i = 1; i < NUM_LUT; i = i+1) begin	
+	generate for (i = 1; i < NUM_LUTS; i = i+1) begin	
             (* dont_touch = "yes" *) LUT1 #(.INIT(2'b10)) buf_lut (.I0(lut_out[i-1]). O(lut_out[i]));
 	end endgenerate
-	assign out = lut_out[NUM_LUT-1];
+	assign out = lut_out[NUM_LUTS-1];
     `endif
 endmodule
 
