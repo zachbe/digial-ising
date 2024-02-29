f743136480e100806303b4ef511e94acbc0a5a58
zbelateche <zachbela@stanford.edu>
2024-02-28 23:09:22 -0800

    Draft of ising lib

---
+++ sw/ising_lib.c
@@ -0,0 +1,145 @@
+
+// A very simple library for interfacing with the Ising Machine!
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <fpga_pci.h>
+#include <fpga_mgmt.h>
+#include <utils/lcd.h>
+
+#include <utils/sh_dpi_tasks.h>
+
+#define START_ADDR       UINT64_C(0x00000500)
+#define CTR_CUTOFF_ADDR  UINT64_C(0x00000600)
+#define CTR_MAX_ADDR     UINT64_C(0x00000700)
+#define PHASE_ADDR       UINT64_C(0x00001000)
+#define WEIGHT_ADDR_BASE UINT64_C(0x01000000)
+
+/* use the stdout logger for printing debug information  */
+const struct logger *logger = &logger_stdout;
+/*
+ * pci_vendor_id and pci_device_id values below are Amazon's and avaliable to use for a given FPGA slot. 
+ * Users may replace these with their own if allocated to them by PCI SIG
+ */
+static uint16_t pci_vendor_id = 0x1D0F; /* Amazon PCI Vendor ID */
+static uint16_t pci_device_id = 0xF000; /* PCI Device ID preassigned by Amazon for F1 applications */
+
+/*
+ * check if the corresponding AFI for hello_world is loaded
+ */
+int check_afi_ready(int slot_id);
+
+void usage(char* program_name) {
+    printf("usage: %s [--slot <slot-id>][<poke-value>]\n", program_name);
+}
+
+//-------------------------------------------------------------------------------------------------------------
+
+pci_bar_handle_t attach(){
+    /* pci_bar_handle_t is a handler for an address space exposed by one PCI BAR on one of the PCI PFs of the FPGA */
+    pci_bar_handle_t pci_bar_handle = PCI_BAR_HANDLE_INIT;
+
+    /* attach to the fpga, with a pci_bar_handle out param
+     * To attach to multiple slots or BARs, call this function multiple times,
+     * saving the pci_bar_handle to specify which address space to interact with in
+     * other API calls.
+     * This function accepts the slot_id, physical function, and bar number
+     */
+    rc = fpga_pci_attach(slot_id, FPGA_APP_PF, APP_PF_BAR0, 0, &pci_bar_handle);
+    return pci_bar_handle;
+}	
+
+int write(int val, int addr) {
+    pci_bar_handle_t pbh;
+    pbh = attach();
+
+    rc = fpga_pci_poke(pbh, val, addr);
+
+    fpga_pci_detach(pbh);
+    return rc;
+}
+
+int read(int addr) {
+    pci_bar_handle_t pbh;
+    pbh = attach();
+
+    uint32_t value;
+    rc = fpga_pci_peek(pbh, addr, &value);
+    
+    fpga_pci_detach(pbh);
+    return value;
+}
+
+//-------------------------------------------------------------------------------------------------------------
+int initialize_fpga() {
+    int slot_id = 0; // Always use slot zero for now!
+    int rc;
+    
+    /* initialize the fpga_mgmt library */
+    rc = fpga_mgmt_init();
+    fail_on(rc, out, "Unable to initialize the fpga_mgmt library");
+
+    rc = check_afi_ready(slot_id);
+    fail_on(rc, out, "AFI not ready");
+    
+    return rc;    
+out:
+    return 1;
+}
+
+//-------------------------------------------------------------------------------------------------------------
+// Provided whole-cloth by AWS. Thanks!
+int check_afi_ready(int slot_id) {
+   struct fpga_mgmt_image_info info = {0}; 
+   int rc;
+
+   /* get local image description, contains status, vendor id, and device id. */
+   rc = fpga_mgmt_describe_local_image(slot_id, &info,0);
+   fail_on(rc, out, "Unable to get AFI information from slot %d. Are you running as root?",slot_id);
+
+   /* check to see if the slot is ready */
+   if (info.status != FPGA_STATUS_LOADED) {
+     rc = 1;
+     fail_on(rc, out, "AFI in Slot %d is not in READY state !", slot_id);
+   }
+
+   printf("AFI PCI  Vendor ID: 0x%x, Device ID 0x%x\n",
+          info.spec.map[FPGA_APP_PF].vendor_id,
+          info.spec.map[FPGA_APP_PF].device_id);
+
+   /* confirm that the AFI that we expect is in fact loaded */
+   if (info.spec.map[FPGA_APP_PF].vendor_id != pci_vendor_id ||
+       info.spec.map[FPGA_APP_PF].device_id != pci_device_id) {
+     printf("AFI does not show expected PCI vendor id and device ID. If the AFI "
+            "was just loaded, it might need a rescan. Rescanning now.\n");
+
+     rc = fpga_pci_rescan_slot_app_pfs(slot_id);
+     fail_on(rc, out, "Unable to update PF for slot %d",slot_id);
+     /* get local image description, contains status, vendor id, and device id. */
+     rc = fpga_mgmt_describe_local_image(slot_id, &info,0);
+     fail_on(rc, out, "Unable to get AFI information from slot %d",slot_id);
+
+     printf("AFI PCI  Vendor ID: 0x%x, Device ID 0x%x\n",
+            info.spec.map[FPGA_APP_PF].vendor_id,
+            info.spec.map[FPGA_APP_PF].device_id);
+
+     /* confirm that the AFI that we expect is in fact loaded after rescan */
+     if (info.spec.map[FPGA_APP_PF].vendor_id != pci_vendor_id ||
+         info.spec.map[FPGA_APP_PF].device_id != pci_device_id) {
+       rc = 1;
+       fail_on(rc, out, "The PCI vendor id and device of the loaded AFI are not "
+               "the expected values.");
+     }
+   }
+    
+   return rc;
+out:
+   return 1;
+}
+//-------------------------------------------------------------------------------------------------------------
